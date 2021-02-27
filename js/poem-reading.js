227b96425db3f007ce95dc1827b3b589cfca02ea
Itzel Contreras <itselfcg@gmail.com>
2021-02-26 17:37:14 -0800

    More poems to read. New JS file functions to manipulate poem preview.

--- js/poem-reading.js
+++ js/poem-reading.js
@@ -1,38 +1,56 @@
-function showPoem() {
-    var x = document.getElementById("poem10");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+
+var idPoemShowed=5;
+var totalItems;
+var currentIndex;
+var down_index;
+
+
+$(document).ready(function() {
+    showPoem(idPoemShowed);
+});
+
+
+function showPoem(id) {
+    var x = document.getElementById("poem"+id);
+    var y;
+
+    for (let i = 1; i <= 5; i++) {
+        if (id === i) {
+            x.style.display = "block"
+            getCurrentItems(id);
+
+        }else {
+            y = document.getElementById("poem"+i)
+            y.style.display = "none";
+
+        }
+    }
+
+}
+
+function previousPart(id) {
+    down_index = down_index - 1;
+    if (down_index >= 1) {
+        document.getElementById('lblPoem' + id).innerHTML = '' + down_index + '/' + totalItems + '';
     }
 }
 
-$(document).ready(function() {
+function nextPart(id) {
+    getCurrentItems(id);
+    currentIndex_active = $('#poem' + id + ' .carousel-item.active').index() + 2;
+    if (totalItems >= currentIndex_active) {
+        down_index = $('#poem' + id + '  .carousel-item.active').index() + 2;
+        document.getElementById('lblPoem' + id).innerHTML = '' + currentIndex_active + '/' + totalItems + '';
+    }
+
+}
+
+function getCurrentItems(id){
+    totalItems = $('#poem' + id + ' .carousel-item').length;
+    currentIndex = $('#poem' + id + ' .carousel-item.active').index() + 1;
+    document.getElementById('lblPoem' + id).innerHTML = '' + currentIndex + '/' + totalItems + '';
+
+}
 
 
-    var totalItems = $('.carousel-item').length;
-    var currentIndex = $('div.carousel-item.active').index() + 1;
 
-    var down_index;
-    $('#lblPoemPart').html('' + currentIndex + '/' + totalItems + '');
-
-    $(".carousel-control-next").click(function () {
-        currentIndex_active = $('div.carousel-item.active').index() + 2;
-        if (totalItems >= currentIndex_active) {
-            down_index = $('div.carousel-item.active').index() + 2;
-            $('#lblPoemPart').html('' + currentIndex_active + '/' + totalItems + '');
-        }
-        console.log(totalItems);
-
-    });
-
-    $(".carousel-control-prev").click(function () {
-        down_index = down_index - 1;
-        if (down_index >= 1) {
-            $('#lblPoemPart').html('' + down_index + '/' + totalItems + '');
-        }
-        console.log(totalItems);
-
-    });
-
-});
